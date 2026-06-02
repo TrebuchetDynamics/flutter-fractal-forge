53da0185032ac78dca117614bb99ad4195aefaab
xel <tamez.jm@gmail.com>
2026-06-02 14:02:42 -0600

    Checkpoint pre-existing changes before auto refactor

--- test/shaders/escape_time_family/catalogs/mandelbrot_variants_shader_assets_test.dart
+++ test/shaders/escape_time_family/catalogs/mandelbrot_variants_shader_assets_test.dart
@@ -4,6 +4,28 @@ import '../../support/shader_asset_expectations.dart';
 
 void main() {
   const shaderRoot = 'shaders/escape_time_family/mandelbrot_variants/';
+  const expectedShaderAssetsByResponsibility = <String, Set<String>>{
+    'exterior_coloring': <String>{
+      'mandelbrot_curvature_avg_gpu.frag',
+      'mandelbrot_de_gpu.frag',
+      'mandelbrot_orbit_trap_gpu.frag',
+      'mandelbrot_stripe_avg_gpu.frag',
+      'mandelbrot_tia_gpu.frag',
+    },
+    'iterative_maps': <String>{
+      'angrybrot_gpu.frag',
+      'crazybrot_gpu.frag',
+      'damaged_doublebrot_gpu.frag',
+      'exp_additive_mandelbrot_gpu.frag',
+      'inverse_mandelbrot_gpu.frag',
+      'lightningbrot_gpu.frag',
+      'simonbrot_gpu.frag',
+      'undefined_gpu.frag',
+    },
+    'singular_perturbations': <String>{
+      'mandelpinski_gpu.frag',
+    },
+  };
   late final Set<String> declaredShaderAssets;
 
   setUpAll(() {
@@ -13,26 +35,21 @@ void main() {
   test('catalog Mandelbrot variant shader assets are declared and present', () {
     final assets = escapeTimeShaderAssetsStartingWith(shaderRoot);
 
-    expect(
-      assets.map((asset) => asset.split('/').last).toSet(),
-      containsAll(<String>{
-        'angrybrot_gpu.frag',
-        'crazybrot_gpu.frag',
-        'damaged_doublebrot_gpu.frag',
-        'exp_additive_mandelbrot_gpu.frag',
-        'inverse_mandelbrot_gpu.frag',
-        'lightningbrot_gpu.frag',
-        'mandelbrot_curvature_avg_gpu.frag',
-        'mandelbrot_de_gpu.frag',
-        'mandelbrot_orbit_trap_gpu.frag',
-        'mandelbrot_stripe_avg_gpu.frag',
-        'mandelbrot_tia_gpu.frag',
-        'mandelpinski_gpu.frag',
-        'simonbrot_gpu.frag',
-        'undefined_gpu.frag',
-      }),
-    );
+    final expectedAssets = expectedShaderAssetsByResponsibility.entries
+        .expand(
+          (entry) => entry.value.map(
+            (asset) => '$shaderRoot${entry.key}/$asset',
+          ),
+        )
+        .toSet();
+    final rootAssets = assets.where((asset) {
+      final relativeAsset = asset.substring(shaderRoot.length);
+      return !relativeAsset.contains('/');
+    }).toList();
 
+    expect(assets.toSet(), containsAll(expectedAssets));
+    expect(rootAssets, isEmpty,
+        reason: 'Mandelbrot variants should live in responsibility subfolders');
     expectAssetsDeclaredAndExist(assets, declaredShaderAssets,
         fileReason: 'file must exist');
   });
