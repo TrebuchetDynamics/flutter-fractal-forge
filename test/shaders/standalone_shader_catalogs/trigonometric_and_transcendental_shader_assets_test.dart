0569011825453b619b2198209532d9ea7d3ac7e6
xel <tamez.jm@gmail.com>
2026-06-02 03:29:42 -0600

    Checkpoint pre-existing changes before auto refactor

--- test/shaders/standalone_shader_catalogs/trigonometric_and_transcendental_shader_assets_test.dart
+++ test/shaders/standalone_shader_catalogs/trigonometric_and_transcendental_shader_assets_test.dart
@@ -4,6 +4,10 @@ import '../support/shader_asset_expectations.dart';
 
 void main() {
   const shaderRoot = 'shaders/trigonometric_and_transcendental/';
+  const elementaryTrigRoot = '${shaderRoot}elementary_trig/';
+  const hyperbolicRoot = '${shaderRoot}hyperbolic/';
+  const exponentialIterationRoot = '${shaderRoot}exponential_iteration/';
+  const specialFunctionsRoot = '${shaderRoot}special_functions/';
   late final Set<String> declaredShaderAssets;
 
   setUpAll(() {
@@ -15,7 +19,21 @@ void main() {
     final assets =
         declaredShaderAssetsStartingWith(declaredShaderAssets, shaderRoot);
 
-    expect(assets, isNotEmpty);
+    expect(assets, hasLength(35));
+    expect(assets.where((asset) => asset.startsWith(elementaryTrigRoot)),
+        hasLength(16));
+    expect(assets.where((asset) => asset.startsWith(hyperbolicRoot)),
+        hasLength(7));
+    expect(assets.where((asset) => asset.startsWith(exponentialIterationRoot)),
+        hasLength(4));
+    expect(assets.where((asset) => asset.startsWith(specialFunctionsRoot)),
+        hasLength(8));
+    expect(
+      assets.where((asset) => asset.substring(shaderRoot.length).contains('/')),
+      hasLength(35),
+      reason:
+          'Trigonometric and transcendental shaders belong in responsibility subfolders',
+    );
     expectAssetsExist(assets);
   });
 
@@ -24,7 +42,18 @@ void main() {
       () {
     final catalogAssets = escapeTimeShaderAssetsStartingWith(shaderRoot);
 
-    expect(catalogAssets, isNotEmpty);
+    expect(catalogAssets, hasLength(35));
+    expect(catalogAssets.where((asset) => asset.startsWith(elementaryTrigRoot)),
+        hasLength(16));
+    expect(catalogAssets.where((asset) => asset.startsWith(hyperbolicRoot)),
+        hasLength(7));
+    expect(
+        catalogAssets
+            .where((asset) => asset.startsWith(exponentialIterationRoot)),
+        hasLength(4));
+    expect(
+        catalogAssets.where((asset) => asset.startsWith(specialFunctionsRoot)),
+        hasLength(8));
     expectAssetsDeclaredAndExist(catalogAssets, declaredShaderAssets);
   });
 }
