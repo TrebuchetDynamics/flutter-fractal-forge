da63fe30988248d24f50220ab03502780188fc33
xel <tamez.jm@gmail.com>
2026-06-01 23:36:11 -0600

    Checkpoint pre-existing changes before auto refactor

--- test/shaders/escape_time_family/families/burning_ship_shader_assets_test.dart
+++ test/shaders/escape_time_family/families/burning_ship_shader_assets_test.dart
@@ -4,6 +4,8 @@ import '../../support/shader_asset_expectations.dart';
 
 void main() {
   const shaderRoot = 'shaders/escape_time_family/families/burning_ship/';
+  const parameterPlaneRoot = '${shaderRoot}parameter_plane/';
+  const juliaSetsRoot = '${shaderRoot}julia_sets/';
   late final Set<String> declaredShaderAssets;
 
   setUpAll(() {
@@ -15,6 +17,15 @@ void main() {
         declaredShaderAssetsStartingWith(declaredShaderAssets, shaderRoot);
 
     expect(assets, hasLength(14));
+    expect(assets.where((asset) => asset.startsWith(parameterPlaneRoot)),
+        hasLength(7));
+    expect(assets.where((asset) => asset.startsWith(juliaSetsRoot)),
+        hasLength(7));
+    expect(
+      assets.where((asset) => asset.substring(shaderRoot.length).contains('/')),
+      hasLength(14),
+      reason: 'Burning Ship shaders belong in responsibility subfolders',
+    );
     expectAssetsExist(assets);
   });
 
@@ -22,6 +33,10 @@ void main() {
     final catalogAssets = escapeTimeShaderAssetsStartingWith(shaderRoot);
 
     expect(catalogAssets, hasLength(14));
+    expect(catalogAssets.where((asset) => asset.startsWith(parameterPlaneRoot)),
+        hasLength(7));
+    expect(catalogAssets.where((asset) => asset.startsWith(juliaSetsRoot)),
+        hasLength(7));
     expectAssetsDeclaredAndExist(catalogAssets, declaredShaderAssets);
   });
 }
